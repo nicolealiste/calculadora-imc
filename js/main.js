6026d05fd43b7c1cb8ecacd64e76239417374ffe
Sagicary <sagicary@gmail.com>
2017-05-09 01:09:13 -0300

    Actualización de Clau

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 function calcularIMC(){
-	
+var num=0;
+var inputP = document.getElementById("peso").value;
+var inputE = document.getElementById("estatura").value;
+var calcula = document.getElementById("mostrar");
+num = (inputP/(inputE*inputE)).toFixed(2);
+calcula.innerHTML = "Su IMC es de <b>"+num+"</b>";
 }
